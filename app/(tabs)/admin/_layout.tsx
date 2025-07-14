de5b923d91d99d61b6a7a2aec37db77c56caccd0
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-07-14 09:31:03 +0800

    update pages

--- app/(tabs)/admin/_layout.tsx
+++ app/(tabs)/admin/_layout.tsx
@@ -1,13 +1,16 @@
 import { Strings } from '@/constants/Strings';
+import { MetadataProvider } from '@/contexts/MetadataContext';
 import { Stack } from 'expo-router';
 
 export default function AdminLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="edit" options={{ title: Strings.adminLayout.editSong }} />
-      <Stack.Screen name="create" options={{ title: Strings.adminLayout.createSong }} />
-	  <Stack.Screen name="search" options={{ title: Strings.adminLayout.search }} />
-    </Stack>
+	<MetadataProvider>
+		<Stack>
+		<Stack.Screen name="index" options={{ headerShown: false }} />
+		<Stack.Screen name="edit" options={{ title: Strings.adminLayout.editSong }} />
+		<Stack.Screen name="create" options={{ title: Strings.adminLayout.createSong }} />
+		<Stack.Screen name="search" options={{ title: Strings.adminLayout.search }} />
+		</Stack>
+	</MetadataProvider>
   );
 }
