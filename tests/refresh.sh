8978157ce6e4f251c39a3e50fda6ee350d452286
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-07-25 16:46:53 +0800

    Add a script to pickup song list from arcade rhythm games open song lists

---
+++ tests/refresh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://maimai.sega.jp/data/maimai_songs.json > maimai_songs.json
+curl https://chunithm.sega.jp/storage/json/music.json > chunithm_songs.json
+curl https://ongeki.sega.jp/assets/json/music/music.json > ongeki_songs.json
